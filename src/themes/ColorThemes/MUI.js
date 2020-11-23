f67c00d2c13d008f513422946229b62dea579ad4
notShadowM <maanabahin2001@gmail.com>
2020-11-23 19:08:36 +0200

    some changes
    #1

--- src/themes/ColorThemes/MUI.js
+++ src/themes/ColorThemes/MUI.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({
+export const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#DA5E42",
